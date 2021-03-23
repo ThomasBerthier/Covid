a2888b22d4b4d10871c620a2d60b35c7f874031a
ThomasBerthier <tberthier@la-providence.net>
2021-03-23 18:12:13 +0100

    Changes

--- db.php
+++ db.php
@@ -9,4 +9,90 @@ try {
 } catch (\Throwable $th) {
     echo $th;
 }
+
+if(isset($DB)){
+    if (isset($_SESSION["Connected"]) && $_SESSION["Connected"]===true){
+        $access = true;
+        $access = afficheFormulaireLogout($DB);
+    }else{
+        $access = false;
+        $access = afficheFormulaireConnexion($DB);
+    }
+   
+}else{
+    echo"base";
+}
+
+function afficheFormulaireLogout($DB){
+    $afficheForm = true;
+    $access = true;
+    if(isset($_POST["deco"])){
+        $_SESSION["Connected"]=false;
+        session_unset();
+        session_destroy();
+        afficheFormulaireConnexion($DB);
+        $afficheForm = false;
+        $access = false;
+    }else{
+        $afficheForm = true;
+    }
+
+    if($afficheForm){
+    ?>
+        <form action="" method="post" >
+            <div >
+                <input type="submit" value="Deconnexion" name="deco">
+            </div>
+        </form>
+
+    <?php
+    
+    }
+
+    return $access;
+}
+
+function afficheFormulaireConnexion($DB){
+
+    $access = false;
+    if( isset($_POST["nom"]) && isset($_POST["mdp"])){
+        $stmt = $DB->prepare("SELECT * FROM User WHERE nom= ? AND mdp = ?");
+        $stmt->execute(array($_POST["nom"], $_POST["mdp"]));
+        if($tab = $stmt->fetch()){ 
+            $access = true;
+            $_SESSION["Connected"]=true;
+            $afficheForm = false;
+            afficheFormulaireLogout($DB);
+        }else{
+            $afficheForm = true;
+        }
+
+                    
+
+    }else{
+        $afficheForm = true;
+    }
+    
+    if($afficheForm){
+    ?>
+        <form action="" method="post" >
+            <div>
+                <label for="nom">nom: </label>
+                <input type="text" name="nom" id="">
+            </div>
+            <div >
+                <label for="mdp">mdp: </label>
+                <input type="password" name="mdp" id="">
+            </div>
+            <div >
+                <input type="submit" value="Connexion" >
+            </div>
+        </form>
+
+    <?php
+    }
+
+    return $access;
+        
+}
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php require 'db.php';
-      session_start(); 
+<?php 
+session_start();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,57 +11,15 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
-        <label for="nom">nom :</label>
-        <input type="text" name="nom" id="">
-        <label for="mdp">msp</label>
-        <input type="text" name="mdp" id="">
-        <button name="submit">connexion</button>
-        <?php 
-        if ((isset($_SESSION)) && (isset($_SESSION["nom"]))) {
-            ?><input type="submit" value="deconnexion" name="deconnexion" id="deco"><?php
-        }
-        ?>
-    </form>
-
+    
     <?php
-        if (isset($_POST["submit"])) {
-            if ((isset($_POST["nom"]) && ($_POST["nom"] != ""))) {
-                if ((isset($_POST["mdp"]) && ($_POST["mdp"] != ""))) {
-                    $stmt = $DB->prepare("SELECT * FROM User WHERE nom = ?");
-                    $stmt->execute(array($_POST["nom"]));
-                    $stmt = $stmt->fetch();
-                    if ($stmt) {
-                        if ($stmt["nom"] == $_POST["nom"]) {
-                            if ($stmt["mdp"] == $_POST["mdp"]) {
-                                echo"bien connecté";  
-                                $_SESSION["nom"] = $stmt["nom"];
-                                $_SESSION["mdp"] = $stmt["mdp"];
-                                header("Refresh:0");
-                            }else{
-                                echo"mdp inconnu";
-                            }
-                        }else{
-                            echo"nom inconnu";
-                        }
-                    }else{
-                        echo"compte inconnu";
-                    }
-                }else{
-                    echo"pas de mot de passe";
-                }
-            }else{
-                echo"pas de nom";
-            }
-        }else{
-            echo"form non envoyé";
-        }
-        if(isset($_SESSION)){
-            if(isset($_POST["deconnexion"])){
-                session_destroy();
-                header("Refresh:0");
-            }
-        }
+    include "db.php"; 
+
+    if($access){
+        echo "ok";
+    }else{
+        echo "Acces au site refusé";
+    }
     ?>
 </body>
 </html>
